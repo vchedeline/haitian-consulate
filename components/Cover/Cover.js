cc36e20850e7fb989a01406d7525fd8ca8433fe4
Chedeline Viljean <vchedeline@gmail.com>
2023-11-02 21:24:37 -0400

    clean page

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export const Cover = ({ children, background }) => {
   return (
-    <div className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center">
+    <div
+      as={"image"}
+      className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center"
+    >
       <Image
         alt="Cover"
         src={background}
         fill
-        priority
+        priority={true}
         className="mix-blend-soft-light object-cover"
       />
       <div className="max-w-5xl z-10">{children}</div>
