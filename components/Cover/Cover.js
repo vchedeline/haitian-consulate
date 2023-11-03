d1020b01f10d3baac7a99d3766823e3df5bf952a
Chedeline Viljean <vchedeline@gmail.com>
2023-11-02 23:09:05 -0400

    remove as

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -2,10 +2,7 @@ import Image from "next/image";
 
 export const Cover = ({ children, background }) => {
   return (
-    <div
-      as={"image"}
-      className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center"
-    >
+    <div className="h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center">
       <Image
         alt="Cover"
         src={background}
