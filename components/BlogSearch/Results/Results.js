6989e8474a7cf3ef8a6bd02c04b1974a0773e25a
Chedeline Viljean <vchedeline@gmail.com>
2023-11-06 22:38:55 -0500

    revised 1.3

--- components/BlogSearch/Results/Results.js
+++ components/BlogSearch/Results/Results.js
@@ -2,7 +2,7 @@ import { BlogCard } from "./BlogCard";
 
 export const Results = ({ blogs }) => {
   return (
-    <div className="max-w-5xl mx-auto grid grid-cols-3 gap-5 mb-10">
+    <div className="max-w-5xl mx-auto grid md:grid-cols-3 gap-5 mb-10">
       {blogs.map((blog) => (
         <BlogCard
           key={blog.databaseId}
